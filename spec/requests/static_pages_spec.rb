8b3c019b0a0675443e5ba59d0148cf95e0cd2adc
Hero <herowhj@gmail.com>
2014-12-03 18:54:22 +0800

    set a css style

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,34 +2,35 @@ require 'rails_helper'
 
 #RSpec.describe "StaticPages", :type => :request do
   describe "Static pages" do
-    let(:base_title) {"My Blog"}
+    #subject{ page }
+    let(:base_title) {"校友微博"}
     describe "Home page" do
-      it "should hava the content 'Blog'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Blog')
-    end
-    it "should have the right title on my home page" do
-      visit 'static_pages/home'
-      expect(page).to have_title("#{base_title}")
-    end 
-  end
+      it "should hava the content '欢迎'" do
+      visit root_path
+        expect(page).to have_content('欢迎')
+      end
+      it "should have the right title on my home page" do
+        visit root_path
+        expect(page).to have_title("#{base_title} | Home")
+      end
+    end  
   describe "Help page" do
     it "should hava the content 'help'" do
-    visit '/static_pages/help'
+    visit help_path
       expect(page).to have_content('help')
     end
     it "should have the right title on my help page" do
-      visit 'static_pages/help'
+      visit help_path
       expect(page).to have_title("#{base_title} | Help")
     end
   end  
   describe "About page" do
     it "should hava the content 'help'" do
-    visit '/static_pages/about'
+    visit about_path
     expect(page).to have_content('about')
     end
     it "should have the right title on my about page" do
-      visit 'static_pages/about'
+      visit about_path
       expect(page).to have_title("#{base_title} | About Us")
     end
   end
@@ -37,11 +38,11 @@ require 'rails_helper'
  
     describe "Contact page" do
       it "should hava the content 'Contact'" do
-      visit '/static_pages/contact'
+      visit contact_path
       expect(page).to have_content('Contact Me')
     end
     it "should have the right title on my contact page" do
-      visit 'static_pages/contact'
+      visit contact_path
       expect(page).to have_title("#{base_title} | Contact")
     end 
     end
